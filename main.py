b3738f863e5904eaee2d23674fe8da55fb08205d
chasenunez <chasenunez@gmail.com>
2025-10-14 14:48:30 +0200

    adding circleshapes code

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def main():
 	
 	screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+	Clock = pygame.time.Clock()
+	dt = 0
+	
+
+
+
+
 	while True:
 		for event in pygame.event.get():
                         if event.type == pygame.QUIT:
@@ -14,6 +21,6 @@ def main():
 		
 		screen.fill("black")
 		pygame.display.flip()
-	
+		dt = (Clock.tick(60)*1000)
 if __name__ == "__main__":
 	main()
