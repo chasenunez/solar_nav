086a14d4a669e63e6932acc96f70291c906b1555
chasenunez <chasenunez@gmail.com>
2025-10-14 16:38:38 +0200

    have a working triangle being drawn in a black field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	print("Starting Asteroids!")
@@ -9,17 +10,17 @@ def main():
 
 	Clock = pygame.time.Clock()
 	dt = 0
+
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
 	
-
-
-
-
 	while True:
 		for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return
-		
+			
 		screen.fill("black")
+		Player.draw(Player(x,y), screen)
 		pygame.display.flip()
 		dt = (Clock.tick(60)*1000)
 if __name__ == "__main__":
