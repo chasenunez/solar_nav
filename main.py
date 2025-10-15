159b3a3676324d53e316a1b7b8adf73c323d1dfe
chasenunez <chasenunez@gmail.com>
2025-10-15 11:06:01 +0200

    working rotation with a and d

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ def main():
 	pygame.init()
 	
 	screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 	Clock = pygame.time.Clock()
+	
 	dt = 0
 
-	x = SCREEN_WIDTH / 2
-	y = SCREEN_HEIGHT / 2
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
 	while True:
 		for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return
 			
+		Player.update(player,dt)
+
 		screen.fill("black")
-		Player.draw(Player(x,y), screen)
+		Player.draw(player, screen)
 		pygame.display.flip()
-		dt = (Clock.tick(60)*1000)
+
+		dt = (Clock.tick(60) / 1000)
+		
 if __name__ == "__main__":
 	main()
