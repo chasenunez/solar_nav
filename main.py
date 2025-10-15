b6049a3b394c1fd80d1bc2446a6f4ce4054dd727
chasenunez <chasenunez@gmail.com>
2025-10-15 13:38:49 +0200

    have chnaged the code to incorperate group for drawable and updatable units

--- main.py
+++ main.py
@@ -11,17 +11,28 @@ def main():
 	
 	dt = 0
 
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	
+	
+
 	
 	while True:
 		for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return
 			
-		Player.update(player,dt)
+		updatable.update(dt)
 
 		screen.fill("black")
-		Player.draw(player, screen)
+		
+		for obj in drawable:
+    			obj.draw(screen)
+    			
 		pygame.display.flip()
 
 		dt = (Clock.tick(60) / 1000)
